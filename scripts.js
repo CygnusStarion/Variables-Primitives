0663a0f3d1ad76d6c886a1d638bdf425f13d9b45
CygnusStarion <54466514+CygnusStarion@users.noreply.github.com>
2020-02-05 12:23:47 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,42 @@
+//var timeOfDay = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var timeOfDay = "Afternoon";
+var greeting = (`Good ${timeOfDay}.`);
+console.log(greeting);
+
+// undefined variable
+var newVariable
+console.log(typeof newVariable);
+
+// typeof = object
+var newVariable = null;
+console.log(typeof newVariable);
+
+// non existing variable is undefined
+console.log(typeof nonExist);
+
+// prints again successfully
+console.log(greeting);
+
+var name = "Mary";
+
+var favoriteSong = "The Voice by The Moody blues";
+
+var wage = "15";
+
+var age = "44";
+
+var onlyChild = "false";
+
+var satisfied = "true";
+
+// prints primitive variable
+var favoriteThing = "Peace";
+console.log(favoriteThing);
+
+var favoriteSong = "The Grudge by Tool";
+
+// typeof is string
+var favoriteThing = "Serenity";
+console.log(typeof favoriteThing);
+
+var age = "45";
